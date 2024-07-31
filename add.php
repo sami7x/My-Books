14c40a77d8ae5d1cd96210dbd06ffe40d94e5084
Sami <samibajra7@gmail.com>
2024-07-31 10:39:32 +0545

    add feature added

---
+++ add.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = $_POST["title"];
+    $author = $_POST["author"];
+    $genre = $_POST["genre"];
+    $status = $_POST["status"];
+
+    $book = "$title, $author, $genre, $status";
+    file_put_contents("books.txt", $book, FILE_APPEND);
+    header("Location: index.php"); //redirects to this page after submitting
+    exit;
+}
+?>
+
+<form method="POST">
+    Title: <input type="text" name="title" required></input>
+    Author: <input type="text" name="author" required></input>
+    Genre: <input type="text" name="genre" required></input>
+    Status:
+    <select name="status">
+        <option value="read">Read</option>
+        <option value="unread">Unread</option>
+        <option value="reading">Reading</option>
+    </select>
+    <input type="Submit">
+</form>
---
+++ view.php
@@ -0,0 +1,25 @@
+<?php
+
+$books = file("books.txt", FILE_IGNORE_NEW_LINES);
+?>
+
+<h1>My Book List</h1>
+<table>
+    <tr>
+        <th>
+            Title
+        </th>
+        <th>
+            Author
+        </th>
+        <th>
+            Genre
+        </th>
+        <th>
+            Status
+        </th>
+    </tr>
+    <tr>
+
+    </tr>
+</table>
